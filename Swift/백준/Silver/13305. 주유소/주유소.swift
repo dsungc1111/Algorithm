4dae69affa980b8e20ef4e2b3a0ff9fb7b7fdb44
dsungc <114575573+dsungc1111@users.noreply.github.com>
2024-12-20 21:53:47 +0900

    [Silver III] Title: 주유소, Time: 56 ms, Memory: 88764 KB, Score: 17 point -BaekjoonHub

---
+++ Swift/백준/Silver/13305. 주유소/주유소.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+func minimumOilPrice() {
+    
+    let numOFCities = Int(readLine()!)!
+    
+    let lengthOfRoad = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    let priceOFOil = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    var min = priceOFOil[0]
+    var result = 0
+    
+    for i in 0..<numOFCities - 1 {
+        
+        if priceOFOil[i] < min {
+            print(priceOFOil[i], min)
+            min = priceOFOil[i]
+        }
+        result += min * lengthOfRoad[i]
+    }
+    
+    
+    print(result)
+}
+minimumOilPrice()
