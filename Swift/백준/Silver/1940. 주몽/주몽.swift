c3d2078a16836f242f35c7890defec3472e8250e
dsungc <114575573+dsungc1111@users.noreply.github.com>
2024-12-22 15:39:37 +0900

    [Silver IV] Title: 주몽, Time: 16 ms, Memory: 80512 KB -BaekjoonHub

--- Swift/백준/Silver/1940. 주몽/주몽.swift
+++ Swift/백준/Silver/1940. 주몽/주몽.swift
@@ -1,34 +1,34 @@
 import Foundation
 
 
-func jumong() {
+func acculatedSum() {
     
     let N = Int(readLine()!)!
     let M = Int(readLine()!)!
-    var materials = readLine()!.split(separator: " ").map { Int($0)! }
     
-    materials.sort()
+    var mat = readLine()!.split(separator: " ").map { Int($0)! }
     
-    var count = 0
+    mat.sort()
+    
     var end = N - 1
+    var count = 0
     var start = 0
-    
-    while start < end {
+   
+    while start < end   {
         
-        let sum = materials[start] + materials[end]
+        let sum = mat[start] + mat[end]
         
-        if sum == M {
+        if sum < M {
+            start += 1
+        } else if sum == M {
             count += 1
             start += 1
             end -= 1
-        } else if sum < M {
-            start += 1
         } else {
             end -= 1
         }
         
     }
-    
     print(count)
 }
-jumong()
+acculatedSum()
