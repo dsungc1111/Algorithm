70baca6db147f8dbfe5e5fefac9cd414431f4e50
dsungc <114575573+dsungc1111@users.noreply.github.com>
2024-12-27 20:11:39 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

---
+++ Swift/백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+
+func fashionKing() {
+  
+    let testCase = Int(readLine()!)!
+   
+    
+    for _ in 0..<testCase {
+        
+        let N = Int(readLine()!)!
+        
+        var categoryCounts = [String : Int]()
+        
+        
+        for _ in 0..<N {
+            let input = readLine()!.split(separator: " ").map { String($0) }
+            let category = input[1]
+
+            categoryCounts[category, default: 0] += 1
+            
+        }
+        
+        let result = categoryCounts.values.reduce(1) { $0 * ($1 + 1) } - 1
+        print(result)
+    }
+    
+    
+}
+
+fashionKing()
