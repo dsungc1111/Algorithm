711573da749bfae82da9f78bf7c20e723e45e81f
dsungc <114575573+dsungc1111@users.noreply.github.com>
2025-01-03 11:08:55 +0900

    [Silver II] Title: 유기농 배추, Time: 16 ms, Memory: 79520 KB -BaekjoonHub

--- Swift/백준/Silver/1012. 유기농 배추/유기농 배추.swift
+++ Swift/백준/Silver/1012. 유기농 배추/유기농 배추.swift
@@ -2,75 +2,82 @@
 import Foundation
 
 
-func organic() {
+func organic3() {
     
+    let T = Int(readLine()!)!
     
+    for _ in 0..<T {
+     test()
+    }
+    
+}
+
+func test() {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let M = input[0], N = input[1], K = input[2]
     let directions = [(1,0), (0,1), (-1,0), (0,-1)]
     
-    let testcase = Int(readLine()!)!
+    var arr = Array(repeating: Array(repeating: 0, count: N), count: M)
+    var vis = Array(repeating: Array(repeating: false, count: N), count: M)
     
-    for _ in 0..<testcase {
-     
-        let input = readLine()!.split(separator: " ").map { Int($0)! }
-        let M = input[0], N = input[1], K = input[2]
-        
-        
-        var field = Array(repeating: Array(repeating: 0, count: N), count: M)
-        var vis = Array(repeating: Array(repeating: false, count: N), count: M)
-        
-        
-        for _ in 0..<K {
-            let baechu = readLine()!.split(separator: " ").map{ Int($0)! }
-            let x = baechu[0]
-            let y = baechu[1]
-            field[x][y] = 1
-            
-        } 
-        struct Position {
-            let x: Int
-            let y: Int
-        }
-     
-        func bfs(_ start:Position) {
-            
-            var queue = QueueForMaze<Position>()
-            queue.enqueue(start)
-            vis[start.x][start.y] = true
-            
-            while !queue.isEmpty {
-                
-                guard let current = queue.dequeue() else { continue }
-                
-                let x = current.x
-                let y = current.y
-                
-                for direction in directions {
-                    let nx = x + direction.0
-                    let ny = y + direction.1
-                    
-                    
-                    if nx >= 0 && nx < M && ny >= 0 && ny < N && !vis[nx][ny] && field[nx][ny] == 1 {
-                        vis[nx][ny] = true
-                        queue.enqueue(Position(x: nx, y: ny))
-                    }
-                }
-            }
-            
-        }
-        var count = 0
-        
-        for i in 0..<M {
-            for j in 0..<N {
-                if field[i][j] == 1 && !vis[i][j] {
-                    bfs(Position(x: i, y: j))
-                    count += 1
-                }
-            }
-        }
-        print(count)
+    for _ in 0..<K {
+        let place = readLine()!.split(separator: " ").map{ Int($0)! }
+        arr[place[0]][place[1]] = 1
     }
+    
+    struct Position {
+        let x: Int
+        let y: Int
+    }
+    func bfs(_ element: Position) {
+        
+        var queue = QueueForOrganic3<Position>()
+        queue.enqueue(element)
+        vis[element.x][element.y] = true
+        
+        
+        while !queue.isEmpty {
+            
+            guard let current = queue.dequeue() else { break }
+            
+            let x = current.x
+            let y = current.y
+            
+            for direction in directions {
+                
+                let nx = x + direction.0
+                let ny = y + direction.1
+                
+                if nx >= 0 && ny >= 0 && nx < M && ny < N && !vis[nx][ny] && arr[nx][ny] == 1 {
+                    queue.enqueue(Position(x: nx, y: ny))
+                    vis[nx][ny] = true
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    var count = 0
+    for x in 0..<M {
+        for y in 0..<N {
+            if !vis[x][y] && arr[x][y] == 1 {
+                bfs(Position(x: x, y: y))
+                count += 1
+            }
+        }
+    }
+    
+    print(count)
+    
 }
-struct QueueForMaze<T> {
+
+
+
+
+
+struct QueueForOrganic3<T> {
     
     private var elements: [T] = []
     
@@ -86,4 +93,4 @@ struct QueueForMaze<T> {
         return isEmpty ? nil : elements.removeFirst()
     }
 }
-organic()
+organic3()
