0dc61c4f7eff3e45bf06316e49fa189c8e08a2a9
dsungc <114575573+dsungc1111@users.noreply.github.com>
2025-01-06 11:59:09 +0900

    [Silver I] Title: 미로 탐색, Time: 12 ms, Memory: 79664 KB -BaekjoonHub

--- Swift/백준/Silver/2178. 미로 탐색/미로 탐색.swift
+++ Swift/백준/Silver/2178. 미로 탐색/미로 탐색.swift
@@ -1,21 +1,17 @@
 
 import Foundation
 
-// 최소한의 수 -> 
-
-func maze2() {
+func maze3() {
     
     let input = readLine()!.split(separator: " ").map { Int($0)! }
-    
     let N = input[0], M = input[1]
-    var arr = [[Int]]()
     
-    let directions = [(1,0), (0,1), (-1,0), (0,-1)]
+    var arr = [[Int]]()
     var vis = Array(repeating: Array(repeating: false, count: M), count: N)
     
-    for _ in 0..<N {
-        arr.append(readLine()!.map{ Int(String($0))! })
-    }
+    for _ in 0..<N { arr.append(readLine()!.map{Int(String($0))!}) }
+    
+    let directions = [(1,0), (-1,0), (0,1), (0,-1)]
     
     struct Position {
         let x: Int
@@ -23,8 +19,7 @@ func maze2() {
         let dis: Int
     }
     
-    var queue = QueueMaze2<Position>()
-    
+    var queue = Maze3Queue<Position>()
     queue.enqueue(Position(x: 0, y: 0, dis: 1))
     vis[0][0] = true
     
@@ -40,33 +35,36 @@ func maze2() {
             print(dis)
             return
         }
+        
+        
         for direction in directions {
-            
             let nx = x + direction.0
             let ny = y + direction.1
             
-            if nx >= 0 && ny >= 0 && nx < N && ny < M && arr[nx][ny] == 1 && !vis[nx][ny] {
+            if nx >= 0 && nx < N && ny >= 0 && ny < M && !vis[nx][ny] && arr[nx][ny] == 1
+            {
                 vis[nx][ny] = true
                 queue.enqueue(Position(x: nx, y: ny, dis: dis + 1))
             }
-            
-            
         }
+        
     }
+    
 }
 
-
-
-
-struct QueueMaze2<T> {
+struct Maze3Queue<T> {
     
     private var elements: [T] = []
     
     var isEmpty: Bool { elements.isEmpty }
     
-    mutating func enqueue(_ element: T) { elements.append(element) }
+    mutating func enqueue(_ element: T) {
+        elements.append(element)
+    }
+    
     mutating func dequeue() -> T? {
         return isEmpty ? nil : elements.removeFirst()
     }
+    
 }
-maze2()
+maze3()
